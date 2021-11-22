30f274dbb92bfed5f5c2ca7425e99a844d8c8e37
fedorden <56299191+fedorden@users.noreply.github.com>
2021-11-22 17:55:48 +0000

    changed quarters to years, added churn CoD

--- main.py
+++ main.py
@@ -6,27 +6,44 @@ permutations = list(itertools.permutations([1, 2, 3, 4, 5, 6, 7, 8]))
 
 input_df = pd.read_csv ('input.csv')
 
-column_names = ["Priorities list", "Total CoD"]
+column_names = ["Priorities list", "Churn CoD", "Total CoD"]
 output_df = pd.DataFrame(columns = column_names)
 
 for permutation in permutations:
     time_in_queue = 0
     total_cod = 0
+    churn_cod = 0
     for item in permutation:
         row = input_df.loc[input_df['#'] == item]
+
         item_dev_estimation = row.at[item-1, 'Preliminary development estimation (weeks)']
-        item_cod_week = row.at[item-1, 'Total CoD week']
+        item_total_cod_week = row.at[item-1, 'Total CoD week']
+        item_chrun_cod_week = row.at[item-1, 'Churn CoD week']
 
         time_in_queue = time_in_queue + item_dev_estimation
-        item_cod = item_cod_week * time_in_queue
+
+        item_churn_cod = item_chrun_cod_week * time_in_queue
+        item_cod = item_total_cod_week * time_in_queue
+
+        churn_cod = churn_cod + int(item_churn_cod)
         total_cod = total_cod + int(item_cod)
 
     output_df = output_df.append({
         "Priorities list" : permutation,
+        "Churn CoD": churn_cod,
         "Total CoD": total_cod
     },ignore_index=True)
         
 output_df.to_csv('output.csv')
+
+print("Minimum Churn CoD:")
+sorted_output = output_df.sort_values(by='Churn CoD',ascending=True)
+print(sorted_output.head(3).to_string(index=False))
+print("...")
+
+print("Minimum Total CoD:")
 sorted_output = output_df.sort_values(by='Total CoD',ascending=True)
-print(sorted_output.head(10))
-sorted_output.head(10).to_csv('short_output.csv')
+print(sorted_output.head(3).to_string(index=False))
+print("...")
+
+# sorted_output.head(10).to_csv('short_output.csv')
